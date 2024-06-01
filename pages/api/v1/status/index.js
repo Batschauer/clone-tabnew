54bae9f49c4e40bf44b8a15b69aa26b394fb9e45
Batschauer <nathan01batschauer@gmail.com>
2024-06-01 16:36:59 -0300

    new way to calculate opened connections.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,30 +3,26 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  try {
-    const result = await database.query(
-      `SELECT
+  const result = await database.query({
+    text: `
+      SELECT
         current_setting('server_version') AS server_version,
         current_setting('max_connections')::int AS max_connections,
-        (SELECT count(*)::int FROM pg_stat_activity WHERE state = 'active') AS used_connections;
+        (SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1) AS used_connections;
     `,
-    );
+    values: [process.env.POSTGRES_DB],
+  });
 
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: result.rows[0].server_version,
-          max_connections: result.rows[0].max_connections,
-          opened_connections: result.rows[0].used_connections,
-        },
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: result.rows[0].server_version,
+        max_connections: result.rows[0].max_connections,
+        opened_connections: result.rows[0].used_connections,
       },
-    });
-  } catch (error) {
-    response.status(500).json({
-      error: "Não foi possível obter as estatisticas do banco de dados.",
-    });
-  }
+    },
+  });
 }
 
 export default status;
